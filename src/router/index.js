c27268e45af739b0761e0471ebb8b7d225f05e46
cn02233 <black41802935@163.com>
2020-01-09 14:00:01 +0800

    FIX

--- src/router/index.js
+++ src/router/index.js
@@ -57,62 +57,7 @@ const routes = [
             name: 'role',
             path: '/sys/role',
             component: () => import('@/models/sys/role/roleMain')
-          },
-          { 
-            name: 'contacts',
-            path: '/system/contacts',
-            component: () => import('@/models/system/contacts/contactsMain')//联系人管理
-          },
-          {
-            name: 'dataConsumptionPlan',
-            path: '/dataexchange/dataConsumptionPlan',
-            component: () => import('@/models/dataexchange/dataConsumptionPlan/consumptionMain')//数据消费计划
-          },
-          {
-            name: 'consumptionUser',
-            path: '/dataexchange/consumptionUser',
-            component: () => import('@/models/dataexchange/dataProductionPlan/consumptionUser')//数据消费方
-          },
-          {
-            name: 'dataProductionPlan',
-            path: '/dataexchange/dataProductionPlan',
-            component: () => import('@/models/dataexchange/dataProductionPlan/productionMain')//数据生产计划
-          },
-          {
-            name: 'dataAdministration',
-            path: '/dataexchange/dataAdministration',
-            component: () => import('@/models/dataexchange/dataAdministration/administrationMain')//数据集管理
-          },
-          {
-            name: 'appProjectAdministration',
-            path: '/system/appProjectAdministration',
-            component: () => import('@/models/system/appProjectAdministration/projectMain')//应用项目管理
-          },
-          {
-            name: 'sysParamsConfig',
-            path: '/system/sysParamsConfig',
-            component: () => import('@/models/system/sysParamsConfig/sysParamsConfigMain')//系统参数配置
-          },
-          {
-            name: 'AgentStatus',
-            path: '/watcher/AgentStatus',
-            component: () => import('@/models/watcher/AgentStatus/agentStatusMain')//Agent状态
-          },
-          {
-            name: 'serverStatus',
-            path: '/watcher/serverStatus',
-            component: () => import('@/models/watcher/serverStatus/serverStatusMain')//server状态
-          },
-          {
-            name: 'transStatusQuery',
-            path: '/watcher/transStatusQuery',
-            component: () => import('@/models/watcher/transStatusQuery/transStatusQueryMain')//传输状态查询
-          },
-          {
-            name: 'transLog',
-            path: '/watcher/transLog',
-            component: () => import('@/models/watcher/transLog/transLogMain')//传输日志
-          },
+          }
         ]
       }
     ]
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,5 +2,5 @@
  * Created by SongCQ on 2018/8/17.
  */
 // export { default as Request } from './request'
-export { default as SemanticAnalysisRequest } from './requestUtil'
-export { default as SemanticAnalysisBaseRequest } from './baseRequestUtil'
+export { default as requestUtil } from './requestUtil'
+export { default as baseRequestUtil } from './baseRequestUtil'
